23bc8f2715a17fbc369934f876b7a293dfde859b
Desock <salvagomezrisen@gmail.com>
2024-10-01 16:47:19 +0200

    ejercicio ficheros

--- copia_seguridad.php
+++ copia_seguridad.php
@@ -1,7 +1,15 @@
 <?php
     $fichero = "./copia_seguridad/datos.txt";
+    $datosFichero = file_get_contents($fichero);
     if (file_exists($fichero)) {
-        if (preg_match(preg_replace("^\w{4,}[@]{1}\w[.]{1}[a-z]{2,4}$", "salvador@gmai.com", "salvagomezrisen@gmail.com");))
+        if (preg_match("^\w{4,}[@]{1}\w{4,}[.]{1}[a-z]{2,4}$", $datosFichero) == 1) {
+            echo "Hay coincidencias.";
+            preg_replace("^\w{4,}[@]{1}\w{4,}[.]{1}[a-z]{2,4}$", "salva@outlook.es", "salvagomezrisen@gmail.com");
+        } else {
+            echo "Patron no encontrado.";
+        }       
+    } else {
+        echo "El fichero no existe.";
     }
 
     //"^\w{4,}[@]{1}\w[.]{1}[a-z]{2,4}$"
